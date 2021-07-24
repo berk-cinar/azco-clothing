c22d70e5a060b2421a7da3fd3c8e6e602a8429a3
Berk Çınar <berkcinr@gmail.com>
2021-07-24 23:49:44 +0300

    homepage updates

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items, item }) => (
+    <div className="collection-preview" >
+        <h1 className="title" >{title.toUpperCase()}</h1>
+        <div className="preview">
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(item => (
+                    <div key={item.id}>{item.name}
+                    </div>
+
+                ))
+
+
+            }
+
+        </div>
+    </div>
+)
+
+export default CollectionPreview;
